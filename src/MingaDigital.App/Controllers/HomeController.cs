feed634d5ee8c385315758123f3e34d33e052e43
Gabriel Garcia <garciat@live.com>
2015-05-15 22:01:15 -0400

    namespace superfluos

--- src/MingaDigital.App/Controllers/HomeController.cs
+++ src/MingaDigital.App/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Linq;
-//using System.Threading.Tasks;
-using System.Collections.Generic;
 
 using Microsoft.AspNet.Mvc;
-
-using MingaDigital.App.EF;
-using MingaDigital.App.Entities;
 
 namespace MingaDigital.App.Controllers
 {
