a5316e4ca78d4dcd1dfda1800365cbec61ea3889
Gabriel Garcia <garciat@live.com>
2015-05-17 21:27:10 -0400

    tipo actividad con vistas genericas

--- src/MingaDigital.App/Controllers/TipoActividadController.cs
+++ src/MingaDigital.App/Controllers/TipoActividadController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-//using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using Microsoft.AspNet.Mvc;
@@ -11,124 +10,65 @@ using MingaDigital.App.Models;
 
 namespace MingaDigital.App.Controllers
 {
-    [Route("tipo-actividad")]
-    public class TipoActividadController : Controller
+    [Route("tipos-actividad")]
+    public class TipoActividadController
+        : BasicCrudController<
+            MainContext,
+            TipoActividad,
+            TipoActividadIndexModel,
+            TipoActividadIndexTableRow,
+            TipoActividadDetailModel,
+            TipoActividadEditorModel
+        >
     {
-        [FromServices]
-        public MainContext Db { get; set; }
-        
-        [HttpGet("")]
-        public IActionResult Index()
+        protected override IEnumerable<TipoActividadIndexTableRow> GetIndexRows(TipoActividadIndexModel model)
         {
             var query =
-                Db.TipoActividad
-                .OrderBy(x => x.Descripcion);
+                CrudSet
+                .Select(x => new TipoActividadIndexTableRow
+                {
+                    TipoActividadId = x.TipoActividadId,
+                    Descripcion = x.Descripcion
+                });
             
             var result = query.ToArray();
             
-            return View(result);
+            return result;
         }
-
-        [HttpGet("nuevo")]
-        public IActionResult Create()
+        
+        protected override TipoActividadDetailModel EntityToDetailModel(TipoActividad entity)
         {
-            return View();
-        }
-
-        [HttpPost("nuevo")]
-        public IActionResult Create([FromForm] TipoActividadEditorModel model)
-        {
-            if (!ModelState.IsValid)
+            return new TipoActividadDetailModel
             {
-                return View(model);
-            }
-            
-            var entity = new TipoActividad
+                TipoActividadId = entity.TipoActividadId,
+                Descripcion = entity.Descripcion
+            };
+        }
+        
+        protected override TipoActividadEditorModel GetInitialEditorModel()
+        {
+            return new TipoActividadEditorModel();
+        }
+        
+        protected override TipoActividadEditorModel EntityToEditorModel(TipoActividad entity)
+        {
+            return new TipoActividadEditorModel
+            {
+                Descripcion = entity.Descripcion
+            };
+        }
+        
+        protected override TipoActividad EditorModelToEntity(TipoActividadEditorModel model)
+        {
+            return new TipoActividad
             {
                 Descripcion = model.Descripcion
             };
-            
-            Db.TipoActividad.Add(entity);
-            
-            Db.SaveChanges();
-            
-            return RedirectToAction("Index");
         }
-
-        [HttpGet("{id}/eliminar")]
-        public IActionResult Delete(Int32 id)
+        
+        protected override void ApplyEditorModel(TipoActividadEditorModel model, TipoActividad entity)
         {
-            return WithEntity(id, entity =>
-            {
-                var model = new TipoActividadDetailModel
-                {
-                    TipoActividadId = entity.TipoActividadId,
-                    Descripcion = entity.Descripcion
-                };
-
-                return View(model);
-            });
-        }
-
-        [HttpGet("{id}/modificar")]
-        public IActionResult Update(Int32 id)
-        {
-            return WithEntity(id, entity =>
-            {
-                var model = new TipoActividadEditorModel
-                {
-                    Descripcion = entity.Descripcion
-                };
-                return View("Create", model);
-            });
-        }
-
-        [HttpPost("{id}/modificar")]
-        public IActionResult Update(Int32 id, [FromForm] TipoActividadEditorModel model)
-        {
-            return WithEntity(id, entity =>
-            {
-                if (!ModelState.IsValid)
-                {
-                    return View("Create", model);
-                }
-
-                entity.Descripcion = model.Descripcion;
-                
-                Db.SaveChanges();
-                
-                return RedirectToAction("Index");
-            });
-        }
-
-        [HttpPost("{id}/eliminar")]
-        public IActionResult DeleteConfirm(Int32 id)
-        {
-            return WithEntity(id, entity =>
-            {
-                Db.TipoActividad.Remove(entity);
-
-                Db.SaveChanges();
-
-                return RedirectToAction("Index");
-            });
-        }
-
-        private IActionResult WithEntity(Int32 id, 
-            Func<TipoActividad, IActionResult> func)
-        {
-            var query =
-                Db.TipoActividad
-                .Where(p => p.TipoActividadId == id);
-            
-            var result = query.FirstOrDefault();
-            
-            if (result == null)
-            {
-                return HttpNotFound();
-            }
-            
-            return func.Invoke(result);
+            entity.Descripcion = model.Descripcion;
         }
     }
 }
--- src/MingaDigital.App/Models/TipoActividadModels.cs
+++ src/MingaDigital.App/Models/TipoActividadModels.cs
@@ -1,16 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 using Microsoft.AspNet.Mvc.Rendering;
 
 namespace MingaDigital.App.Models
 {
-    public class TipoActividadDetailModel : TipoActividadEditorModel
+    [Description("Tipos de Actividad")]
+    public class TipoActividadIndexModel : BasicIndexModel<TipoActividadIndexTableRow>
+    {
+    }
+    
+    public class TipoActividadIndexTableRow
     {
         public Int32 TipoActividadId { get; set; }
+        
+        [Display(Name = "Descripción")]
+        public String Descripcion { get; set; }
     }
-
+    
+    [Description("Tipo de Actividad")]
+    public class TipoActividadDetailModel
+    {
+        public Int32 TipoActividadId { get; set; }
+        
+        [Display(Name = "Descripción")]
+        public String Descripcion { get; set; }
+    }
+    
+    [Description("Tipo de Actividad")]
     public class TipoActividadEditorModel
     {
         [Required(ErrorMessage = "{0} es un campo requerido.")]
