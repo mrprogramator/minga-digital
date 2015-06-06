c8e4496011cdce29cff81ef8049cc2a1e2bce7f2
Gabriel Garcia <garciat@live.com>
2015-06-05 23:11:24 -0400

    No intentar crear base de datos (debuggeando heroku)

--- src/MingaDigital.App/EF/NpgsqlEFConfiguration.cs
+++ src/MingaDigital.App/EF/NpgsqlEFConfiguration.cs
@@ -3,16 +3,25 @@ using System;
 using System.Data.Common;
 using System.Data.Entity;
 using System.Data.Entity.Migrations.History;
+using System.Data.Entity.Core.Metadata.Edm;
 
 using Npgsql;
 
 namespace Npgsql
 {
+    public class CustomNpgsqlServices : NpgsqlServices
+    {
+        protected override void DbCreateDatabase(DbConnection connection, int? commandTimeout, StoreItemCollection storeItemCollection)
+        {
+            // skip
+        }
+    }
+    
     public class NpgsqlEFConfiguration : DbConfiguration
     {
         public NpgsqlEFConfiguration()
         {
-            SetProviderServices("Npgsql", NpgsqlServices.Instance);
+            SetProviderServices("Npgsql", new CustomNpgsqlServices());
             SetProviderFactory("Npgsql", NpgsqlFactory.Instance);
             SetDefaultConnectionFactory(new NpgsqlConnectionFactory());
             SetMigrationSqlGenerator("Npgsql", () => new NpgsqlMigrationSqlGenerator());
